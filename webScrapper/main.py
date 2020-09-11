8a2f33391e7c1c8b44c7574ebd9b21eefe76124f
Diego Rojas <Diego-27ro@outlook.com>
2020-09-11 18:18:49 -0500

    scrapper Finalizado

--- webScrapper/main.py
+++ webScrapper/main.py
@@ -1,8 +1,22 @@
 import argparse
 import logging
+import re  #importo el modulo de expresiones regulares
+
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+is_Well_formed_link = re.compile(r'^https?://.+/.+$')
+"""^ nos da el inicio de la palabra 
+     r dice que es un string row
+     es http con s opcional
+     .+/ queremos una o mas letras y que termine en /
+     $ significa terminar el patron"""
+is_root_path = re.compile(r'^/.+$')
 
 import news_pages_objects as news
-logging.basicConfig(level = logging.INFO)
 from common import config
 
 logger = logging.getLogger(__name__)
@@ -12,7 +26,37 @@ def _news_scraper(news_site_uid):
 
     logging.info('Begining scrapper for {}'.format(host))
     homepage = news.HomePage(news_site_uid,host)
-    print(homepage.article_links)
+
+    articles = []
+    for link in homepage.article_links:
+        article = _fetch_article(news_site_uid,host,link)
+        if article:
+            logger.info('Article fetched!!!')
+            articles.append(article)
+            print("TITULO DEL ARTICULO: ",article.title)
+        print(len(articles))
+
+def _fetch_article(news_site_uid,host,link):
+    logging.info('Start fetching article at {}'.format(link))
+    article = None
+
+    try:
+        article = news.ArticlePage(news_site_uid,_build_link(host,link))
+    except(HTTPError,MaxRetryError) as e:
+        logger.warning("Error while fetching the article",exc_info = False)
+
+    if article and not article.body:
+        logger.warning('Invalid article. There is no body')
+    return article
+
+def _build_link(host,link):
+    if is_Well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{}{}'.format(host,link)
+    else:
+        return'{host}/{uri}'.format(host=host,uri = link)
+
 
 
 if __name__ == '__main__':
--- webScrapper/news_pages_objects.py
+++ webScrapper/news_pages_objects.py
@@ -3,38 +3,51 @@ import bs4
 
 from common import config
 
-"""class NewsPage:
+class NewsPage:
     def __init__(self,idSitioNoticias,url):
         self._config = config()['news_sites'][idSitioNoticias]
         self._queries = self._config['queries']
         self._html = None
         self._visit(url)
-"""
 
-class HomePage():
-    
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+    def _visit(self, url):
+        response = requests.get(url)
+        response.raise_for_status()  # arroja si la solicitud no se concluye correctamente
+
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+class HomePage(NewsPage):
+
     def __init__(self,idSitioNoticias,url):
-        self._config = config()['news_sites'][idSitioNoticias]
-        self._queries = self._config['queries']
-        self._html = None
-
-        self._visit(url)
-        #super().__init__(idSitioNoticias,url)
+        super().__init__(idSitioNoticias,url)
 
     @property
     def article_links(self):
         link_list = []
         for link in self._select(self._queries['homepage_article_links']): ##accedo al diccionario
-            if link and link.has_attr('href'): # select me retorna un objeto, revisar que atributos y metodos tienen los objetos que retorna select
+            if link and link.has_attr('href'):
                 link_list.append(link)
-        print(link_list)
-        return set(link_list)
+        return set(link['href'] for link in link_list)
 
-    def _select(self,query_string):
-        return self._html.select(query_string)
+class ArticlePage(NewsPage):
 
-    def _visit(self,url):
-        response = requests.get(url)
-        response.raise_for_status() #arroja si la solicitud no se concluye correctamente
+    def __init__(self,news_site_uid,url):
+        super().__init__(news_site_uid,url)
 
-        self._html = bs4.BeautifulSoup(response.text,'html.parser')
+
+    #En web scrapp hay que ser muy defensivos
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
+
+
