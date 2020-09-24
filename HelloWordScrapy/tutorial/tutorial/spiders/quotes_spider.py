4338a0b838b3389f530b99be5b59ac3312adde3d
Diego Rojas <Diego-27ro@outlook.com>
2020-09-23 22:48:04 -0500

    practicing create and run projects with scrapy

--- HelloWordScrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ HelloWordScrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -1,17 +1,30 @@
 import scrapy
 
+
 class QuotesSpider(scrapy.Spider):
+    #name es el nombre unico con el quescrapy se va a referir al spider
     name='quotes' #esta variable tiene que llamarse name
-    _start_urls =[
-        'http://quotes.toscrape.com/'
+    start_urls =[
+        "http://quotes.toscrape.com/"
     ]
+
 
     def parse(self,response):
 
-        """define la logica a partir de la cual extraemos info"""
-        with open('resultados.html','w+',encoding = 'utf-8') as f:
-            f.write(response.text)
-
-
+        print("ENTRA A LA FUNCION PARSE")
+        #with open('resultados.html', 'w', encoding='utf-8') as f:
+        #    f.write(response.text)
+        title = response.xpath('// h1 / a / text()').get()
+        print("Title: {}".format(title))
+        print("*" * 10, "\n")
+        quotes = response.xpath('//span[@class="text" and @itemprop = "text"]/text()').getall()
+        print("Quotes: ")
+        for quote in quotes:
+            print("Quote: {}".format(quote),"\n")
+        print("*" * 10)
+        topTenTags = response.xpath('//div[contains(@class,"tags-box")]/span[contains(@class,"tag-item")]/a/text()').getall()
+        for tag in topTenTags:
+            print("Tag: {}".format(tag), "\n")
+        print("*" * 10)
 
 
